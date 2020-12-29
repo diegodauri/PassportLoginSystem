92f475473b384d0fc4941c2d8766e6a954df0e39
diegodauri <diegodauri@users.noreply.github.com>
2020-12-29 19:12:23 +0100

    this should break

--- config/auth.js
+++ config/auth.js
@@ -1,6 +1,6 @@
 module.exports = {
   ensureAuthenticated: function(req, res, next) {
-    if (req.isAuthenticated()) {
+    if (req.isAuthenticate()) {
       return next();
     } else {
       req.flash("error", "Please log in to view this resource");
