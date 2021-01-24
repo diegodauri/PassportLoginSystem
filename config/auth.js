7ae3f7feff1302dbb375d7ff0aa9076895e10098
diegodauri <71179921+diegodauri@users.noreply.github.com>
2021-01-24 19:37:27 +0100

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -1,6 +1,6 @@
 module.exports = {
   ensureAuthenticated: function(req, res, next) {
-    if (req.isAuthenticate()) {
+    if (req.isAuthenticated()) {
       return next();
     } else {
       req.flash("error", "Please log in to view this resource");
